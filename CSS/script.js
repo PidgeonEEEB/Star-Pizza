80ce664332cad8118ff82c0d1c7cabc01d7e797a
PidgeonEEEB <Paviaman2002@hotmail.com>
2025-03-20 09:20:45 +0100

    the java

---
+++ CSS/script.js
@@ -0,0 +1,35 @@
+function fetchData()
+{
+    fetch('CSS/Star_pizza.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Http error" + response.status)
+        }
+        return response.json();
+    })
+    .then(data => createManyPizzas(data))
+    .catch(error => console.error("failed to fetch data", error))
+}
+fetchData();
+const Star_pizza = document.getElementById("Pizza_Menu");
+function createPizza(Pizza_menu){
+    return `
+    <li>
+        ${Pizza_menu.Pizza}
+        <figure>
+            <a href=" ${Pizza_menu.image.big_image}" target="_blank" >
+                <img src="${Pizza_menu.image.small_image}" width="180" height="150" alt="${Pizza_menu.Pizza}" title="${Pizza_menu.Pizza}"> 
+            </a>
+            <figcaption>${Pizza_menu.description}</figcaption>
+        </figure>
+    </li>`;
+}
+
+function createManyPizzas(data)
+{
+    console.log("wtf!?")
+    for (let i = 0; i< data.Pizza_menu.length; i++)
+    {
+        Star_pizza.innerHTML += createPizza(data.Pizza_menu[i])
+    }
+}
